7143c6ef8325b1f15de301567c9de1088e14c901
Conor McShane <Cpmcshane@icloud.com>
2022-11-08 15:49:03 -0700

    Add search by title.

--- SeedData.cs
+++ SeedData.cs
@@ -1,6 +1,48 @@
-﻿namespace MvcMovie
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
 {
-    public class SeedData
+    public static void Initialize(IServiceProvider serviceProvider)
     {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+        {
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "Meet the Mormons",
+                    ReleaseDate = DateTime.Parse("2014-10-10"),
+                    Genre = "Documentary",
+                    Price = 7.99M
+                },
+                new Movie
+                {
+                    Title = "The RM",
+                    ReleaseDate = DateTime.Parse("2003-1-31"),
+                    Genre = "Comedy",
+                    Price = 8.99M
+                },
+                new Movie
+                {
+                    Title = "Sons of Provo",
+                    ReleaseDate = DateTime.Parse("2004-6-19"),
+                    Genre = "Comedy",
+                    Price = 9.99M
+                }
+            );
+            context.SaveChanges();
+        }
     }
 }
