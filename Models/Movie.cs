20c7b223a52bf2efc92784f119ad3f3cd2611ffc
Conor McShane <Cpmcshane@icloud.com>
2022-11-08 18:04:39 -0700

    Reverting

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models;
 
@@ -7,11 +6,8 @@ public class Movie
 {
     public int Id { get; set; }
     public string? Title { get; set; }
-
-    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
-    [Column(TypeName = "deciman(18, 2)")]
     public decimal Price { get; set; }
 }
